ac98f146f5cc20a7d366205891fd9177facb6b7f
Vijaya H <Vijaya.hadimani23@gmail.com>
2025-05-28 00:42:43 +0000

    Added HR Employee Dim objects

---
+++ models/staging/stg_hr_employees.sql
@@ -0,0 +1,28 @@
+{{ 
+    config( materialized='view'
+    )
+}}
+with emp as (
+    SELECT * FROM {{ source('HR_DATA', 'HR_EMPLOYEES') }},
+    ),
+    dept as (
+    SELECT * FROM {{source ('HR_DATA', 'HR_DEPARTMENTS') }}
+    )
+,
+employee as (
+    select
+        emp.employee_id,
+        emp.first_name,
+        emp.last_name,
+        emp.email,
+        emp.phone_number,
+        emp.hire_date,
+        emp.manager_id,
+        emp.salary,
+        dept.department_name,
+        current_timestamp as created_when
+        from emp
+        left join dept on emp.department_id = dept.DEPARTMENT_ID
+    --where _fivetran_deleted = false  -- exclude deleted records
+)
+select * from employee
---
+++ snapshots/employee_snapshot.sql
@@ -0,0 +1,17 @@
+{% snapshot employee_snapshot %}
+
+{{
+    config(
+        target_schema='stage',
+        unique_key='employee_id',
+        strategy='check',
+        check_cols=['department_name'],
+        invalidate_hard_deletes=True
+    )
+}}
+
+select
+    *
+from {{ ref('stg_hr_employees') }}
+
+{% endsnapshot %}
